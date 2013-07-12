4c50b06b3c684b67a55ae89c1711d033b9dd2d41
wbvg-mac <will@mylittleshowpony.com>
2013-07-12 13:02:46 +1000

    Fixed looping error

--- db/migrate/20130710085440_remove_user_id_from_hates_table.rb
+++
@@ -1,5 +0,0 @@
-class RemoveUserIdFromHatesTable < ActiveRecord::Migration
-  def change
-      remove_column :hates, :user_id
-  end
-end
--- db/migrate/20130710090457_add_users_hates_table.rb
+++
@@ -1,8 +0,0 @@
-class AddUsersHatesTable < ActiveRecord::Migration
-  def change
-    create_table :hates_users, :id => false do |t|
-      t.integer :hate_id
-      t.integer :user_id
-    end
-  end
-end
