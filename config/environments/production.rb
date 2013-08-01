a12d999ff55da34a358a842e4d7b10f0fac7590a
wbvg-mac <will@mylittleshowpony.com>
2013-08-01 16:13:49 +1000

    compile environment to true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,7 +19,9 @@ config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+
+  ## MAKE THIS TRUE WBVG
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
